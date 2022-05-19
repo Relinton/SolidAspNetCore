fa6c41f967e0a00db4ccb324b327dedcac008460
Relinton <relintonproande@gmail.com>
2022-05-18 23:37:15 -0400

    Solid com c# no Asp Net Core

---
+++ solid-csharp-master/src/Alura.LeilaoOnline.WebApp/Services/IAdminService.cs
@@ -0,0 +1,18 @@
+﻿
+using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Services
+{
+    public interface IAdminService
+    {
+        IEnumerable<Categoria> ConsultarCategorias();
+        IEnumerable<Leilao> ConsultarLeiloes();
+        Leilao ConsultarLeilaoPorId(int id);
+        void CadastrarLeilao(Leilao leilao);
+        void ModificarLeilao(Leilao leilao);
+        void RemoverLeilao(Leilao leilao);
+        void IniciarPregaoDoLeilaoComId(int id);
+        void FinalizarPregaoDoLeilaoComId(int id);
+    }
+}
---
+++ solid-csharp-master/src/Alura.LeilaoOnline.WebApp/Services/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Services
+{
+    public interface IProdutoService
+    {
+        IEnumerable<Leilao> PesquisarLeiloesEmPregaoPorTermo(string termo);
+        IEnumerable<CategoriaComInfoLeilao> ConsultarCategoriasComTotalDeLeiloesEmPregao();
+        Categoria ConsultarCategoriaPorIdComLeiloesEmPregao(int id);
+    }
+}
